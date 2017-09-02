6fdd5d0712c0cb4fbd5ec1b3ab6c21d1df9a0498
Andrew Berezovskyi <andriib@kth.se>
2017-09-02 22:08:48 +0200

    Add Store Update
    This patch is moved from Gerrit Change 95016.
    
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

---
+++ src/store-update/src/main/java/org/eclipse/lyo/tools/store/update/OSLCMessage.java
@@ -0,0 +1,31 @@
+package org.eclipse.lyo.tools.store.update;
+
+import java.net.URI;
+
+/**
+ * OSLCMessage is .
+ *
+ * @author Andrew Berezovskyi (andriib@kth.se)
+ * @version $version-stub$
+ * @since 0.0.0
+ */
+public class OSLCMessage implements ServiceProviderMessage {
+    private final URI serviceProviderId;
+
+    public OSLCMessage(URI serviceProviderId) {
+        this.serviceProviderId = serviceProviderId;
+    }
+
+    @Override
+    public URI getServiceProviderId() {
+        return serviceProviderId;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("OSLCMessage{");
+        sb.append("serviceProviderId='").append(serviceProviderId).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ src/store-update/src/main/java/org/eclipse/lyo/tools/store/update/change/Change.java
@@ -0,0 +1,34 @@
+package org.eclipse.lyo.tools.store.update.change;
+
+import org.eclipse.lyo.oslc4j.core.model.AbstractResource;
+
+/**
+ * Change is .
+ *
+ * @author Andrew Berezovskyi (andriib@kth.se)
+ * @version $version-stub$
+ * @since 0.0.0
+ */
+public class Change<M> {
+    private final AbstractResource resource;
+    private final HistoryResource historyResource;
+    private final M message;
+
+    public Change(AbstractResource resource, HistoryResource historyResource, M message) {
+        this.resource = resource;
+        this.historyResource = historyResource;
+        this.message = message;
+    }
+
+    public AbstractResource getResource() {
+        return resource;
+    }
+
+    public HistoryResource getHistoryResource() {
+        return historyResource;
+    }
+
+    public M getMessage() {
+        return message;
+    }
+}
